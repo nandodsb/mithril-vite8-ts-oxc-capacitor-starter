a076f31baba7493cc1cee758f56e48c4b7805997
nandodsb <nandosbx@gmail.com>
2026-05-04 22:25:35 -0300

    Update

--- src/Toggle.tsx
+++ src/Toggle.tsx
@@ -11,5 +11,8 @@ export const Toggle = withHooks(({ isOn }: ToggleProps) => {
   const [isTurnedOn, setIsTurnedOn] = useState<boolean>(false);
 
   return (
-    m('button', {class: isTurnedOn ? "btn btn-wide btn-accent" : 'btn btn-wide btn-error', onclick: () => setIsTurnedOn(current => !current)}, isTurnedOn ? 'On' : 'Off') 
+    m('button', {class: isTurnedOn ? "btn btn-wide btn-accent" : 
+      'btn btn-wide btn-error',
+       onclick: () => setIsTurnedOn(current => !current)},
+        isTurnedOn ? 'On' : 'Off') 
 )});
