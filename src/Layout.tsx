6b6fb89540d12b76d9097a23e3f416bc8ea580d3
nandodsb <nandosbx@gmail.com>
2026-05-06 10:35:08 -0300

    Update

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,7 +2,7 @@ import m, { Component } from "mithril";
 
 export const Layout: Component = {
   view: (vnode) => {
-    return m("div", { class: "min-h-screen grid grid-col justify-center  md:flex" }, [
+    return m("template", { class: "min-h-screen grid grid-col justify-center  md:flex" }, [
       m("aside", { class: "invisible w-full bg-base-200 md:visible md:h-screen md:w-1/4" }, "Left"),
       m("main", { class: "w-full h-full p-2 md:w-2/4 md:h-screen" }, vnode.children),
       m(
